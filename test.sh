cd96a6691a35854086bcae193653eff339587a92
Niels Robin-Aubertin <git@niels.fr>
2023-08-26 11:44:35 -0400

    Improve test script

--- test.sh
+++ test.sh
@@ -4,16 +4,7 @@ args="rnqknrpppppp............PPPPPPRNQKNR w"
 
 j=0
 while [ $j -le 20 ]; do
-  args="$(./alamos.py $args)"
-  echo "$args"
-  j=$(( j + 1 ))
-done
-
-args="rnqknrpppppp............PPPPPPRNQKNR w"
-
-j=0
-while [ $j -le 20 ]; do
-  args="$(./alamos $args)"
+  args="$($1 $args)"
   echo "$args"
   j=$(( j + 1 ))
 done
